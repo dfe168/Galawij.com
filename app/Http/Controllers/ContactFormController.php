27c865a0cbda66468601c515123978e3535e0bf4
Dara Kaban <dkaban@gmail.com>
2025-06-09 01:37:42 +0200

    Pref: sending email with queue

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -4,20 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactFormRequest;
 use App\Mail\ContactFormMail;
-use App\Mail\ContactMail;
 use App\Models\ContactForm;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Mail;
 use Inertia\Inertia;
 
 class ContactFormController extends Controller
 {
-    public function send(ContactFormRequest $request)
+    public function send(ContactFormRequest $request): RedirectResponse
     {
         $validated = $request->validated();
 
         ContactForm::create(attributes: $validated);
 
-        Mail::to($validated["email"])->send(new ContactFormMail(contact: $validated));
+        Mail::to($validated["email"])->queue(new ContactFormMail(contact: $validated));
 
         return back()->with('message', 'Your message has been sent. Thank you!');
     }
